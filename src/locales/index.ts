76f4bcf36821f8d51177148eaf6db12bec1572b4
belljun3395 <195850@jnu.ac.kr>
2025-06-30 10:36:27 +0900

    feat: 로케일 선언

---
+++ src/locales/index.ts
@@ -0,0 +1,14 @@
+export const messages = {
+  ko: {
+    message: {
+      hello: "안녕하세요",
+      welcome: "환영합니다",
+    },
+  },
+  en: {
+    message: {
+      hello: "Hello",
+      welcome: "Welcome",
+    },
+  },
+};
