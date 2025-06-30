4ab8e4cbb4de17dafa61cb0ae190109915de498b
belljun3395 <195850@jnu.ac.kr>
2025-06-30 17:20:56 +0900

    feat: 로그인을 위한 메시지 선언

--- src/locales/index.ts
+++ src/locales/index.ts
@@ -1,14 +1,22 @@
 export const messages = {
   ko: {
     message: {
-      hello: "안녕하세요",
-      welcome: "환영합니다",
+      "login-page-title": "로그인 페이지",
+      "login-form-title": "사용자 로그인",
+      "input-email": "이메일을 입력해 주세요.",
+      "input-name": "이름을 입력해 주세요.",
+      "login-button": "로그인",
+      "login-loading": "로그인 중...",
     },
   },
   en: {
     message: {
-      hello: "Hello",
-      welcome: "Welcome",
+      "login-page-title": "Login Page",
+      "login-form-title": "User Login",
+      "input-email": "Enter your email.",
+      "input-name": "Enter your name.",
+      "login-button": "Login",
+      "login-loading": "Logging in...",
     },
   },
 };
