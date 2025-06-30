11a60bfa77d81d3cc67a9cf04665105562aecb94
belljun3395 <195850@jnu.ac.kr>
2025-06-30 17:23:20 +0900

    feat: LoginPage 구현

---
+++ src/views/LoginPage.vue
@@ -0,0 +1,18 @@
+<template>
+  <div>
+    <h1 class="page-header">{{ $t("message.login-page-title") }}</h1>
+    <LoginForm></LoginForm>
+  </div>
+</template>
+
+<script>
+import LoginForm from "@/components/LoginForm.vue";
+
+export default {
+  components: {
+    LoginForm,
+  },
+};
+</script>
+
+<style></style>
