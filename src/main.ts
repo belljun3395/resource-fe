dd1aac75261ddbc30654fc2d110aef4870b3db03
belljun3395 <195850@jnu.ac.kr>
2025-06-30 17:24:49 +0900

    refactor: app에 필요 설정 추가

--- src/main.ts
+++ src/main.ts
@@ -1,11 +1,26 @@
 import { createApp } from "vue";
+import { createPinia } from "pinia";
+import { createI18n } from "vue-i18n";
 import Antd from "ant-design-vue";
-import 'ant-design-vue/dist/reset.css';
+import "ant-design-vue/dist/reset.css";
 
 import App from "./App.vue";
+import router from "./router";
+import { messages } from "./locales";
 
 const app = createApp(App);
+const pinia = createPinia();
 
+const i18n = createI18n({
+  legacy: false,
+  locale: "ko",
+  fallbackLocale: "en",
+  messages,
+});
+
+app.use(pinia);
+app.use(i18n);
 app.use(Antd);
+app.use(router);
 
-app.mount("#app");
+app.mount("#root");
