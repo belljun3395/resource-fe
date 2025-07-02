9e08dbaf027141f357e491391bf123442abcdace
belljun3395 <195850@jnu.ac.kr>
2025-07-02 16:46:55 +0900

    test: LoginForm 스토리 개선

--- src/components/stories/LoginForm.stories.ts
+++ src/components/stories/LoginForm.stories.ts
@@ -1,5 +1,13 @@
 // @ts-nocheck
 import LoginForm from "@/components/LoginForm.vue";
+import { createI18n } from "vue-i18n";
+import { messages } from "@/locales/index";
+
+const i18n = createI18n({
+  locale: "ko",
+  fallbackLocale: "en",
+  messages,
+});
 
 export default {
   title: "Components/LoginForm",
@@ -21,11 +29,30 @@ const Template = (args: any) => ({
       });
     return { args, onLogin };
   },
+  mounted() {
+    // Loading 스토리에서만 샘플 데이터 채우기
+    if (args.fillSampleData) {
+      setTimeout(() => {
+        const emailInput = this.$el.querySelector('input[type="email"]');
+        const nameInput = this.$el.querySelector('input[type="text"]');
+        if (emailInput) {
+          emailInput.value = "admin@company.com";
+          emailInput.dispatchEvent(new Event('input'));
+        }
+        if (nameInput) {
+          nameInput.value = "관리자";
+          nameInput.dispatchEvent(new Event('input'));
+        }
+      }, 100);
+    }
+  },
   template: `
-    <LoginForm
-      v-bind="args"
-      @login="onLogin"
-    />
+    <div style="padding: 20px; background-color: #f5f5f5; min-height: 100vh;">
+      <LoginForm
+        v-bind="args"
+        @login="onLogin"
+      />
+    </div>
   `,
 });
 
@@ -33,8 +60,27 @@ export const Default = Template.bind({});
 Default.args = {
   loading: false,
 };
+Default.decorators = [
+  () => ({
+    setup() {
+      return {};
+    },
+    template: '<div><story /></div>',
+    i18n,
+  }),
+];
 
 export const Loading = Template.bind({});
 Loading.args = {
   loading: true,
+  fillSampleData: true,
 };
+Loading.decorators = [
+  () => ({
+    setup() {
+      return {};
+    },
+    template: '<div><story /></div>',
+    i18n,
+  }),
+];
