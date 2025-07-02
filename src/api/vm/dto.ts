5634da1bcc3450f4711b08cbca39068064298a74
belljun3395 <195850@jnu.ac.kr>
2025-07-02 16:25:20 +0900

    feat: VM 요청 구현

---
+++ src/api/vm/dto.ts
@@ -0,0 +1,27 @@
+import type { PowerStatusString } from "@/types/vm";
+
+export interface VmSource {
+  type: string;
+  id: number;
+  name: string;
+}
+
+export interface VmFlavor {
+  id: number;
+  name: string;
+  description: string;
+  memory: number;
+  rootDisk: number;
+  vcpu: number;
+}
+
+export interface VmInstanceApiResponse {
+  id: number;
+  name: string;
+  description: string;
+  alias: string;
+  powerStatus: PowerStatusString;
+  host: string;
+  source: VmSource;
+  flavor: VmFlavor;
+}
---
+++ src/api/vm/index.ts
@@ -0,0 +1,2 @@
+export * from "@/api/vm/api";
+export * from "@/api/vm/dto";
