5d4b9ff8302ddada01fc89d25482287dc43d41cd
belljun3395 <195850@jnu.ac.kr>
2025-07-06 09:51:42 +0900

    feat: VM 생성을 위한 api 구현

--- src/api/vm/api.ts
+++ src/api/vm/api.ts
@@ -1,6 +1,9 @@
 import { instance } from "@/api/common";
 import type { ApiResponse } from "@/api/common";
-import type { VmInstanceApiResponse } from "@/api/vm/dto";
+import type {
+  VmInstanceApiResponse,
+  VmInstanceCreateRequest,
+} from "@/api/vm/dto";
 
 export const vmApi = {
   async getInstance(
@@ -8,6 +11,23 @@ export const vmApi = {
   ): Promise<VmInstanceApiResponse> {
     const response = await instance.get<ApiResponse<VmInstanceApiResponse>>(
       `/api/v1/servers/instances/${instanceId}`
+    );
+    return response.data.data;
+  },
+
+  async createInstance(
+    data: VmInstanceCreateRequest
+  ): Promise<VmInstanceApiResponse> {
+    const response = await instance.post<ApiResponse<VmInstanceApiResponse>>(
+      `/api/v1/servers/instances`,
+      {
+        name: data.name,
+        description: data.description,
+        host: "192.168.1.1",
+        flavorId: data.flavorId,
+        sourceType: "IMAGE",
+        sourceId: data.imageId,
+      }
     );
     return response.data.data;
   },
