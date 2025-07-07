3780982dcaf476eec9dd40828ec7e40ea8dab35d
belljun3395 <195850@jnu.ac.kr>
2025-07-07 22:37:11 +0900

    feat: VM 필터링 설정 삭제 반영

--- src/components/vm/index.ts
+++ src/components/vm/index.ts
@@ -1,3 +1,2 @@
 export { default as VmInstanceDetails } from "./VmInstanceDetails.vue";
 export { default as VmListTable } from "./VmListTable.vue";
-export { default as VmSearchFilters } from "./VmSearchFilters.vue";
