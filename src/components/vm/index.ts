3815262ef1b438e786f523676e9cac46fc066ad3
belljun3395 <195850@jnu.ac.kr>
2025-07-07 17:41:24 +0900

    feat: VmListTable 컴포넌트 구현

--- src/components/vm/index.ts
+++ src/components/vm/index.ts
@@ -1,1 +1,3 @@
 export { default as VmInstanceDetails } from "./VmInstanceDetails.vue";
+export { default as VmListTable } from "./VmListTable.vue";
+export { default as VmSearchFilters } from "./VmSearchFilters.vue";
