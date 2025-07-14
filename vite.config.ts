b5828363897e6e7e004ba26173e34cb7a760401c
belljun3395 <195850@jnu.ac.kr>
2025-07-14 14:15:07 +0900

    feat: 배포를 위한 액션 추가

--- vite.config.ts
+++ vite.config.ts
@@ -5,6 +5,7 @@ import { resolve } from "path";
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [vue()],
+  base: "/resource-fe/",
   resolve: {
     alias: {
       "@": resolve(__dirname, "src"),
